0b736ba6771e5d96047b0be06e23bec3893be63f
Kylasam NA <42716803+kylasam@users.noreply.github.com>
2023-12-28 22:34:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,74 @@
 from google.cloud import bigquery
-from google.cloud import bigquery
-from google.oauth2 import service_account
-import os
-# Set up your Google Cloud project ID and BigQuery dataset and table information
-project_id = "kylash-edw"
-dataset_id = "dbt_kna"
-table_id = "test_inserts"
 
-# Create a BigQuery client
+def print_current_timestamp():
+    # Initialize the BigQuery client
+    client = bigquery.Client()
 
-import google.auth
+    # Construct the SQL query to get the current timestamp
+    query = "SELECT CURRENT_TIMESTAMP() as current_time"
 
-credentials, project = google.auth.default()
-# credentials = service_account.Credentials.from_service_account_file(os.environ.get('GOOGLE_GHA_CREDS_PATH'))
-print("STARTING THE SCRIPT")
-client = bigquery.Client()
-print("GOT BQ CLIENT",client)
+    # Execute the query
+    query_job = client.query(query)
+
+    # Get the results
+    results = query_job.result()
+
+    # Print the current timestamp
+    for row in results:
+        print("Current Timestamp from BigQuery:", row['current_time'])
+
+# Call the function to print the current timestamp
+print_current_timestamp()
+
+
+
+# from google.cloud import bigquery
+# from google.cloud import bigquery
+# from google.oauth2 import service_account
+# import os
+# # Set up your Google Cloud project ID and BigQuery dataset and table information
+# project_id = "kylash-edw"
+# dataset_id = "dbt_kna"
+# table_id = "test_inserts"
+
+# # Create a BigQuery client
+
+# import google.auth
+
+# credentials, project = google.auth.default()
+# # credentials = service_account.Credentials.from_service_account_file(os.environ.get('GOOGLE_GHA_CREDS_PATH'))
+# print("STARTING THE SCRIPT")
+# client = bigquery.Client()
+# print("GOT BQ CLIENT",client)
 
 
 
 
-# Printing all environment variables
-for key, value in os.environ.items():
-    print(f'{key}======>>>>>>>>>: {value}')
+# # Printing all environment variables
+# for key, value in os.environ.items():
+#     print(f'{key}======>>>>>>>>>: {value}')
 
-print("ENV key for the GCP service account is")
-print(os.environ.get('GOOGLE_GHA_CREDS_PATH'))
+# print("ENV key for the GCP service account is")
+# print(os.environ.get('GOOGLE_GHA_CREDS_PATH'))
 
-print("SPREPRE ROWS")
+# print("SPREPRE ROWS")
 
-# Define the rows to be inserted
-rows_to_insert = [
-    {"name": "Alice", "age": 30, "gender": "Female"},
-    {"name": "Bob", "age": 25, "gender": "Male"},
-    {"name": "Charlie", "age": 35, "gender": "Male"}
-]
+# # Define the rows to be inserted
+# rows_to_insert = [
+#     {"name": "Alice", "age": 30, "gender": "Female"},
+#     {"name": "Bob", "age": 25, "gender": "Male"},
+#     {"name": "Charlie", "age": 35, "gender": "Male"}
+# ]
 
-# Get the BigQuery table
-table_ref = client.dataset(dataset_id).table(table_id)
-table = client.get_table(table_ref)
+# # Get the BigQuery table
+# table_ref = client.dataset(dataset_id).table(table_id)
+# table = client.get_table(table_ref)
 
-print("INSERT ROWS")
-# Insert the rows into the BigQuery table
-errors = client.insert_rows(table, rows_to_insert)
+# print("INSERT ROWS")
+# # Insert the rows into the BigQuery table
+# errors = client.insert_rows(table, rows_to_insert)
 
-if errors == []:
-    print("Records inserted successfully.")
-else:
-    print("Errors occurred while inserting records:", errors)
+# if errors == []:
+#     print("Records inserted successfully.")
+# else:
+#     print("Errors occurred while inserting records:", errors)
